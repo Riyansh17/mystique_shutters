888c1f3ede6e6b8bb9422c52e128494a1b634358
Riyansh Ajmera <ajmerariyansh17@gmail.com>
2025-04-09 22:36:10 +0530

    /
    /

---
+++ src/app/films/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      films
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/sections/FeaturedStories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FeaturedStories = () => {
+  return (
+    <div>
+      Featured
+    </div>
+  )
+}
+
+export default FeaturedStories
